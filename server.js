fdfe24da5fbba11466297a035dd5dea1b8b39e20
Ghostwan <ghostwan@gmail.com>
2020-03-31 12:44:19 +0200

    mutualize clients update functions, improve naming

--- server.js
+++ server.js
@@ -1,100 +1,105 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/views/index.html');
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`VDOC server listenning on ${PORT}`);
 });
 
-app.get('/connectroom/', (req, res) => {
-  res.render('client.ejs', {data: req.query['roomID']});
+app.get("/", (req, res) => {
+  console.log('Requesting index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-io.on('connection', (socket) => {
-   socket.on('connectRoom', (room) => {
-     socket.join(room);
-     var users = Object.keys(io.sockets.adapter.rooms[room].sockets)
-     io.sockets.in(room).emit('connectToRoom', users);
-   })
+app.get("/connectroom/", (req, res) => {
+  console.log(`Connecting to room ${req.query["roomID"]}`);
+  res.render("client.ejs", { data: req.query["roomID"] });
+});
 
-   socket.on('getDeck', (data) => {
-     var deck = newDeck(data['options']);
-     deck = shuffleDeck(deck, deck.length);
-     io.sockets.in(data['room']).emit('onUpdateDeck', {'deck' : deck, 'options' : data['options']});
-   })
+io.on("connection", socket => {
+  socket.on("connectRoom", room => {
+    // console.log(`User ${} connected to room ${room} `);
+    socket.join(room);
+    var users = Object.keys(io.sockets.adapter.rooms[room].sockets);
+    io.sockets.in(room).emit("connectToRoom", users);
+  });
 
-   socket.on('clearPlayingArea', (data) => {
-    io.sockets.in(data['room']).emit('onAreaCleared');
-  })
+  socket.on("getDeck", data => {
+    var deck = newDeck(data["options"]);
+    deck = shuffleDeck(deck, deck.length);
+    io.sockets.in(data["room"]).emit("onUpdateData", { deck: deck, options: data["options"] });
+  });
 
-   socket.on('takeCards', (data) => {
-     cards = takeCards(data[0]['numCards'], data[0]['deck'], data[0]['mycards']);
-     console.log(cards);
-     io.to(data[0]['user']).emit('onUpdateMyCards', cards['mycards']);
-     io.sockets.in(data[0]['room']).emit('onUpdateDeck', cards['deck']);
-   })
+  socket.on("clearPlayingArea", data => {
+    io.sockets.in(data["room"]).emit("onUpdateData", {pile: []});
+  });
 
+  socket.on("takeCards", data => {
+    cards = takeCards(data[0]["numCards"], data[0]["deck"], data[0]["hand"]);
+    io.to(data[0]["user"]).emit("onUpdateHand", cards["hand"]);
+    io.sockets.in(data[0]["room"]).emit("onUpdateData", {deck: cards["deck"]});
+  });
 
-   socket.on('distribute', (data) => {
-    deck = data[0]['deck']
-    numCards = data[0]['numCards']
-    var users = data[0]['users']
+  socket.on("distribute", data => {
+    deck = data[0]["deck"];
+    numCards = data[0]["numCards"];
+    var users = data[0]["users"];
 
-    if(numCards == -1) {
-      numCards = Math.trunc(deck.length / users.length)
+    if (numCards == -1) {
+      numCards = Math.trunc(deck.length / users.length);
     }
 
-    for(var u = 0; u < users.length; u++) {
-      myCards = []
-      result = takeCards(numCards, deck, myCards);
-      deck = result['deck']
-      myCards = result['mycards']
+    for (var u = 0; u < users.length; u++) {
+      hand = [];
+      result = takeCards(numCards, deck, hand);
+      deck = result["deck"];
+      hand = result["hand"];
 
-      io.to(users[u]).emit('onUpdateMyCards', myCards);
+      io.to(users[u]).emit("onUpdateHand", hand);
     }
-    io.sockets.in(data[0]['room']).emit('onUpdateDeck', deck);
-  })
+    io.sockets.in(data[0]["room"]).emit("onUpdateData", {deck: deck});
+  });
 
+  socket.on("shuffleDeck", data => {
+    data = {deck: shuffleDeck(data["deck"], data["deck"].length)}
+    io.sockets.in(data["room"]).emit("onUpdateData", data);
+  });
 
-   socket.on('shuffleDeck', (data) => {
-     io.sockets.in(data['room']).emit('onUpdateDeck', shuffleDeck(data['deck'], data['deck'].length));
-   })
+  socket.on("resetGame", data => {
+    var deck = newDeck(data["options"]);
+    deck = shuffleDeck(deck, deck.length);
+    io.sockets.in(data["room"]).emit("onUpdateData", {deck: deck, hand: []});
+  });
 
-   socket.on('resetGame', (data) => {
-    var deck = newDeck(data['options']);
-    deck = shuffleDeck(deck, deck.length)
-    io.sockets.in(data['room']).emit('onUpdateDeck', deck);
-    io.sockets.in(data['room']).emit('onAreaCleared');
-    io.sockets.in(data['room']).emit('onUpdateMyCards', []);
-  })
-
-   socket.on('updatePile', (data) => {
-    io.sockets.in(data['room']).emit('onUpdatePile', {'pile' : data['pile'], 'options' : data['options']});
-  })
+  // Broadcast function, sync datas a cross all client from a room
+  socket.on("updateData", data => {
+    io.sockets.in(data["room"]).emit("onUpdateData", data );
+  });
 });
 
 function newDeck(options) {
-  var rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  if(options['cavaliers']) {
-      rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J','C','Q', 'K'];
+  var rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  if (options["cavaliers"]) {
+    rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "C", "Q", "K"];
   }
-  var suit = ['Clubs', 'Diamonds', 'Spades', 'Hearts'];
+  var suit = ["Clubs", "Diamonds", "Spades", "Hearts"];
   var cards = [];
   for (var i = 0; i < suit.length; i++) {
     for (var j = 0; j < rank.length; j++) {
-      var card = {"rank" : rank[j], "suit" : suit[i]};
+      var card = { rank: rank[j], suit: suit[i] };
       cards.push(card);
     }
   }
   return cards;
 }
 
-function takeCards(numcards, deck, mydeck) {
+function takeCards(numcards, deck, hand) {
   for (var i = 0; i < numcards; i++) {
-    mydeck.push(deck[0]);
+    hand.push(deck[0]);
     deck.splice(0, 1);
   }
-  data = {"mycards" : mydeck, "deck" : deck};
+  data = { hand: hand, deck: deck };
   return data;
 }
 
@@ -105,10 +110,7 @@ function shuffleDeck(deck, num) {
     shuffledDeck.push(deck[index]);
     deck.splice(index, 1);
   }
-  return shuffledDeck
+  return shuffledDeck;
 }
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-   console.log(`Virtual desk of cards listenning on ${ PORT }`);
-});
+
